0af768ca60b851f61ee8cf7a616c697a1de6b7dd
roberto <rcastellong98@gmail.com>
2025-09-05 10:52:57 +0200

    test

--- mern_blog_app/client/src/componets/Header.js
+++ mern_blog_app/client/src/componets/Header.js
@@ -71,7 +71,7 @@ const Header = () => {
   return (
     <AppBar position="sticky" sx={{ background: `${theme.bg}` }}>
       <Toolbar>
-        <Typography variant="h4">Mariela Hello</Typography>
+        <Typography variant="h4">BlogsApp</Typography>
         {isLoggedIn && (
           <Box display="flex" marginLeft={'auto'} marginRight="auto">
             <Tabs textColor="inherit" value={value} onChange={handleTabChange}>
